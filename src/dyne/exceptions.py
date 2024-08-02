5679ff945695cd6ced0bcf144abdb699c45df3c9
Tabot Kevin <tabot.kevin@gmail.com>
2024-08-02 01:42:20 +0100

    fixed annotation

--- src/dyne/exceptions.py
+++ src/dyne/exceptions.py
@@ -1,8 +1,12 @@
+from typing import Dict, Optional
+
 from starlette.exceptions import HTTPException
 
 
 def abort(
-    status_code: int, detail: str | None = None, headers: dict[str, str] | None = None
+    status_code: int,
+    detail: Optional[str] = None,
+    headers: Optional[Dict[str, str]] = None,
 ) -> None:
     raise HTTPException(
         status_code=status_code, detail=detail, headers=headers
